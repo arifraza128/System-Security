af2222e1ecf78af5d2f388b990447e957e40d7c2
Arif Raza <arif3raza@gmail.com>
2026-08-24 22:11:10 +0530

    Update passwordstrength.py

--- passwordstrength.py
+++ passwordstrength.py
@@ -1,1 +1,27 @@
+import re
 
+password = input("Enter password: ")
+
+score = 0
+
+if len(password) >= 8:
+    score += 1
+
+if re.search(r"[A-Z]", password):
+    score += 1
+
+if re.search(r"[a-z]", password):
+    score += 1
+
+if re.search(r"[0-9]", password):
+    score += 1
+
+if re.search(r"[@#$%&*!]", password):
+    score += 1
+
+if score == 5:
+    print("Strong Password")
+elif score >= 3:
+    print("Medium Password")
+else:
+    print("Weak Password")
